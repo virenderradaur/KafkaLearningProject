c58eba4bdbb36fb643b88ca979e3e28c901a2936
Veer <veer@Veers-MacBook-Pro.local>
2020-09-10 23:02:13 +0530

    Initial commit

---
+++ src/main/java/com/veer/kafka/ProducerDemo.java
@@ -0,0 +1,46 @@
+package com.veer.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class ProducerDemo {
+    public static final String bootstrapServer = "localhost:9092";
+    public static final String topicName = "my_topic";
+    public static void main(String[] args) {
+        System.out.println("Start of main method");
+        // Producer properties
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.put("batch.size", 16384);
+        producerProperties.put("retries", 0);
+        producerProperties.put("linger.ms", 1);
+        producerProperties.put("session.timeout.ms", "10000");
+        // Construct Producer
+        KafkaProducer<String,String> kafkaProducer = new KafkaProducer<String, String>(producerProperties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(topicName,"hello from app");
+           //send
+        try {
+            System.out.println("Sending record...");
+            kafkaProducer.send(producerRecord).get();
+        System.out.println("Sent..");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        kafkaProducer.flush();
+        System.out.println("Closing ...!!!");
+        kafkaProducer.close(10L, TimeUnit.MILLISECONDS);
+    }
+}
